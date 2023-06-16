8806eab899cc8aba3dde1083e05a2e0a48931648
Yash Seth <yashseth2002@gmail.com>
2023-06-16 20:36:31 +0530

    feat: style navbar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <div className="navbar-main">
-      <div className="navbar-header">Marpu Foundation</div>
+      <Link to="/" style={{ textDecoration: 'none', color: 'black' }}><div className="navbar-header">Marpu Foundation</div></Link>
       <div className="navbar-controls">
-        <Link to="/" style={{ textDecoration: 'none', color: 'black' }}><button>Home</button></Link>
-        <Link to="/about-us" style={{ textDecoration: 'none', color: 'black' }}><button>About</button></Link>
-        <Link to="/missions" style={{ textDecoration: 'none', color: 'black' }}><button>Missions</button></Link>
-        <Link to="/login" style={{ textDecoration: 'none', color: 'black' }}><button>Login</button></Link>
+        <Link to="/" style={{ textDecoration: 'none', color: 'black' }}><button className='nav-controls-btns'>Home</button></Link>
+        <Link to="/about-us" style={{ textDecoration: 'none', color: 'black' }}><button className='nav-controls-btns'>About</button></Link>
+        <Link to="/missions" style={{ textDecoration: 'none', color: 'black' }}><button className='nav-controls-btns'>Missions</button></Link>
+        <Link to="/login" style={{ textDecoration: 'none', color: 'black' }}><button className='nav-controls-btns'>Login</button></Link>
       </div>
     </div>
   );
