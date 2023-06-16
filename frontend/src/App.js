9679679424364d6620455b820f5f980074453b14
Yash Seth <yashseth2002@gmail.com>
2023-06-16 20:12:16 +0530

    feat: set up basic routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      Frontend is served
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<h1>Homepage</h1>}></Route>
+          <Route exact path="/about-us" element={<h1>About</h1>}></Route>
+          <Route exact path="/missions" element={<h1>Missions</h1>}></Route>
+          <Route exact path="/login" element={<h1>Login</h1>}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
