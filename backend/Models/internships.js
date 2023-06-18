d9995cd850d2b7f203abfc6e6bfd00811454b7e3
Yash Seth <yashseth2002@gmail.com>
2023-06-18 13:36:39 +0530

    chore: update interships model

--- backend/Models/internships.js
+++ backend/Models/internships.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 const internshipSchema = new mongoose.Schema({
-    ID: String,
     status: String,
     internID: String,
     certificateURL: String,
