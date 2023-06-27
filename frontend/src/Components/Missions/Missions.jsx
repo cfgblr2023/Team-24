3f4d36cd83dc761d839d400f11c596c289f2fec3
Yash Seth <yashseth2002@gmail.com>
2023-06-27 13:04:35 +0530

    feat: add filter for ongoing missions

--- frontend/src/Components/Missions/Missions.jsx
+++ frontend/src/Components/Missions/Missions.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import "./Missions.css"
+import Switch from '@mui/material/Switch';
 
 function Missions() {
     const [missions, setMissions] = useState([])
+    const [missionState, setMissionState] = useState("all")
 
     const getMissions = async (missionState) => {
-        await axios.post("http://127.0.0.1:5000/api/get-missions",{missionState: "ongoing"})
+        await axios.post("http://127.0.0.1:5000/api/get-missions",{missionState: missionState})
         .then((missionsList) => setMissions(missionsList.data))
         .catch((e) => console.log("There is an error with the backend. Please refresh or try again later if the problem persists."))
     }
@@ -37,6 +39,17 @@ function Missions() {
         setSearchField(e.target.value);
       };
     
+    const label = { inputProps: { 'aria-label': 'Switch demo' } };
+
+    const handleToggle = () => {
+        if(missionState === "all") {
+            getMissions("ongoing");
+            setMissionState("ongoing")
+        } else {
+            getMissions();
+            setMissionState("all")
+        }
+    }
   return (
     <div className='missions-main'>
         <div className='missions-list'>
@@ -47,6 +60,10 @@ function Missions() {
           onChange = {handleChange}
         />
         <h3>Missions List</h3>
+        <div className='toggle-switch'>
+            <p>Ongoing</p>
+            <Switch {...label} onClick={() => handleToggle()}/>
+        </div>
         {filteredMissions.map((mission, index) => {
             return (
                 <div key={index} className='mission'>
