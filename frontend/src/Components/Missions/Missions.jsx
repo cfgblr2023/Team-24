54c42e433e2cad18692eaff667d39c8212834a97
Yash Seth <yashseth2002@gmail.com>
2023-06-27 12:48:29 +0530

    feat: add search missions functionality

--- frontend/src/Components/Missions/Missions.jsx
+++ frontend/src/Components/Missions/Missions.jsx
@@ -16,11 +16,56 @@ function Missions() {
     useEffect(() => {
         getMissions()
     }, [])
+
+    const [searchField, setSearchField] = useState("");
+    const filteredMissions = missions.filter(
+        mission => {
+          return (
+            mission
+            .name
+            .toLowerCase()
+            .includes(searchField.toLowerCase()) ||
+            mission
+            .about
+            .toLowerCase()
+            .includes(searchField.toLowerCase())
+          );
+        }
+      );
+
+      const handleChange = e => {
+        setSearchField(e.target.value);
+      };
     
   return (
     <div className='missions-main'>
         <div className='missions-list'>
+        <input 
+          className="pa3 bb br3 grow b--none bg-lightest-blue ma3"
+          type = "search" 
+          placeholder = "Search Mission" 
+          onChange = {handleChange}
+        />
         <h3>Missions List</h3>
+        {filteredMissions.map((mission, index) => {
+            return (
+                <div key={index} className='mission'>
+                <div>
+                    <b>Mission Name: </b>{mission.name}
+                </div>
+                <div>
+                    <b>About: </b>{mission.about}
+                </div>
+                <div>
+                    <b>Type: </b>{mission.type}
+                </div>
+                <div>
+                    <b>Vacancy: </b>{mission.vacancy}
+                </div>
+            </div>
+            )
+        })}
+        {/* <h3>Missions List</h3>
         {missions.map((mission, index) => {
             return(
             <div key={index} className='mission'>
@@ -37,7 +82,7 @@ function Missions() {
                     <b>Vacancy: </b>{mission.vacancy}
                 </div>
             </div>
-        )})}
+        )})} */}
         </div>
     </div>
   )
