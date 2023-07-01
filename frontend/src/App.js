2d6ac604c643bcb2b40dbe0cc6b18548204a7e2d
lagantg19 <lagantg19@gmail.com>
2023-07-01 19:23:08 +0530

    commited to lagan added chat feature added payment gateway for fund raiser

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Authentication from "./components/ChatApplication/Authentication"
+import ChatsPage from "./components/ChatApplication/ChatsPage"
+import FundRaiser from "./components/FundRaiser";
+
 
 function App() {
   return (
     <Router>
-      <div>
+        <Navbar/>
         <Routes>
           <Route path="/home" element={<Home />} />
+          <Route path="/authentication" element={<Authentication />} />
+          <Route path="/chats" element={<ChatsPage />} />
+          <Route path="/fund" element={<FundRaiser />} />
+
+          
         </Routes>
-      </div>
+      
     </Router>
   );
 }
