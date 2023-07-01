65774616ad5006a3b48e2776cff622b92fc27968
lagantg19 <lagantg19@gmail.com>
2023-07-01 16:11:41 +0530

    First setup of Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,13 @@
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar/Navbar";
-import Login from "./Components/Login/Login";
-import Missions from "./Components/Missions/Missions";
-import { useState } from "react"
-import Dashboard from "./Components/Dashboard/Dashboard";
-import About from "./Components/About/About";
+
+import Home from "./components/Home";
 
 function App() {
-  const [isInternAuthenticated, setIsInternAuthenticated] = useState(false)
-  const [currentIntern, setCurrentIntern] = useState({});
   return (
     <Router>
-      <Navbar isInternAuthenticated={isInternAuthenticated} setIsInternAuthenticated={setIsInternAuthenticated}/>
       <div>
         <Routes>
-          <Route exact path="/" element={<h1>Homepage</h1>}></Route>
-          <Route exact path="/about-us" element={<About />}></Route>
-          <Route exact path="/missions" element={<Missions />}></Route>
-          <Route exact path="/login" element={<Login setIsInternAuthenticated={setIsInternAuthenticated} setCurrentIntern={setCurrentIntern} />}></Route>
-          <Route exact path="/dashboard" element={<Dashboard isInternAuthenticated={isInternAuthenticated} currentIntern={currentIntern} setCurrentIntern={setCurrentIntern} />}></Route>
+          <Route path="/home" element={<Home />} />
         </Routes>
       </div>
     </Router>
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>hey</div>;
+};
+
+export default Home;
