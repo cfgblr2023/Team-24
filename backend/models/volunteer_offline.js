4adb03bf4bc7da6bcb7b431a403cfb72cb69d0ed
Yash Seth <yashseth2002@gmail.com>
2023-07-02 01:58:36 +0530

    feat: add schema

---
+++ backend/models/volunteer_offline.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+const volunteerOfflineSchema = new mongoose.Schema({
+    name: String,
+    desc: String,
+    email: String,
+    contact: String,
+    location: String,
+    date: String,
+    slot: String
+});
+
+module.exports = OfflineClass = mongoose.model('offlineClasses', volunteerOfflineSchema);
