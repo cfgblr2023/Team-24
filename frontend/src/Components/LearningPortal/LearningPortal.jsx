345144556df3dfe5eff144ccd5a029d893dafc99
Yash Seth <yashseth2002@gmail.com>
2023-07-01 19:02:22 +0530

    feat: add resources for current course

--- frontend/src/Components/LearningPortal/LearningPortal.jsx
+++ frontend/src/Components/LearningPortal/LearningPortal.jsx
@@ -31,45 +31,100 @@ function LearningPortal() {
           </button>
           <button onClick={() => toggleView("list")}>View Courses</button>
           <button onClick={() => toggleView("compelted")}>Completed</button>
-            <div className="course-list">
-                {currentStudentView === "list" && (
-                <>
-                        {data.map((course, index) => {
-                        return (
-                            <div className="course-details" key={index}>
-                            <p><b>Course ID:</b>{course.courseID}</p>
-                            <p><b>Course Name:</b>{course.courseName}</p>
-                            <p>
-                                <button
-                                onClick={() => {
-                                    setCurrentCourse(course.courseName);
-                                }}
-                                >
-                                Enroll
-                                </button>
-                            </p>
-                            <iframe
-                                width="400"
-                                height="200"
-                                src={`https://www.youtube.com/embed/UGFZcD7NYZk`}
-                                frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowFullScreen
-                                title="Embedded youtube"
-                            />
-                            </div>
-                        );
-                        })}
-                </>
-                )}
-            </div>
-            {currentStudentView === "enrolled" && (
-            <>
-                <div>
-                <p>You are currently enrolled {currentCourse}</p>
-                </div>
-            </>
+          <div className="course-list">
+            {currentStudentView === "list" && (
+              <>
+                {data.map((course, index) => {
+                  return (
+                    <div className="course-details" key={index}>
+                      <p>
+                        <b>Course ID:</b>
+                        {course.courseID}
+                      </p>
+                      <p>
+                        <b>Course Name:</b>
+                        {course.courseName}
+                      </p>
+                      <p>
+                        <button
+                          onClick={() => {
+                            setCurrentCourse(course.courseName);
+                          }}
+                        >
+                          Enroll
+                        </button>
+                      </p>
+                      <iframe
+                        width="400"
+                        height="200"
+                        src={`https://www.youtube.com/embed/UGFZcD7NYZk`}
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                        title="Embedded youtube"
+                      />
+                    </div>
+                  );
+                })}
+              </>
             )}
+          </div>
+          {currentStudentView === "enrolled" && (
+            <>
+              <div className="course-resources">
+                <p>You are currently enrolled to course: <b>{currentCourse}</b></p>
+                <p>
+                  <b>Resources</b>
+                </p>
+                <ul>
+                  <li>
+                    <iframe
+                      width="400"
+                      height="200"
+                      src={`https://www.youtube.com/embed/UGFZcD7NYZk`}
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                      title="Embedded youtube"
+                    />
+                  </li>
+                  <li>
+                    <iframe
+                      width="400"
+                      height="200"
+                      src={`https://www.youtube.com/embed/UGFZcD7NYZk`}
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                      title="Embedded youtube"
+                    />
+                  </li>
+                  <li>
+                    <iframe
+                      width="400"
+                      height="200"
+                      src={`https://www.youtube.com/embed/UGFZcD7NYZk`}
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                      title="Embedded youtube"
+                    />
+                  </li>
+                  <li>
+                    <iframe
+                      width="400"
+                      height="200"
+                      src={`https://www.youtube.com/embed/UGFZcD7NYZk`}
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                      title="Embedded youtube"
+                    />
+                  </li>
+                </ul>
+              </div>
+            </>
+          )}
         </>
       )}
       {currentView === "volunteer" && (
